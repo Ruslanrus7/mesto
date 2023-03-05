70f377ea762b9957073b3a28a42d6fe3150192d3
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-03-06 02:12:14 +0700

    feat: добавить адаптив

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,48 @@
+const popupElement = document.querySelector('.popup');
+const popupEditButton = document.querySelector('.profile__info-button');
+const popupCloseButton = popupElement.querySelector('.popup__close');
 
+
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
+}
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+
+}
+
+
+const closePopupByClickOnOverlay = function(event) {
+  if (event.target === event.currentTarget)
+  closePopup();
+}
+
+
+popupEditButton.addEventListener('click', openPopup)
+
+
+popupCloseButton.addEventListener('click', closePopup);
+
+
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+
+const formElement = popupElement.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__form-name');
+let jobInput = formElement.querySelector('.popup__form-job');
+let nameUser = document.querySelector('.profile__info-name');
+let jobUser = document.querySelector('.profile__info-text');
+const formButton = popupElement.querySelector('.popup__form-btn');
+
+nameInput.value = nameUser.textContent;
+jobInput.value = jobUser.textContent;
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  nameUser.textContent = nameInput.value;
+  jobUser.textContent = jobInput.value;
+  closePopup();
+}
+
+formButton.addEventListener('click', handleFormSubmit);
