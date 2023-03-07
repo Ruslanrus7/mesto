18b30e6e8debc8838eda05dee4ee95cf77cd1f6f
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-03-07 20:18:07 +0700

    fixed: исправлены замечания

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,12 @@ const popupCloseButton = popupElement.querySelector('.popup__close');
 
 const openPopup = function () {
   popupElement.classList.add('popup_opened');
+  nameInput.value = nameUser.textContent;
+  jobInput.value = jobUser.textContent;
 }
 
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
-
 }
 
 
@@ -29,14 +30,11 @@ popupElement.addEventListener('click', closePopupByClickOnOverlay);
 
 
 const formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__form-name');
-let jobInput = formElement.querySelector('.popup__form-job');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 let nameUser = document.querySelector('.profile__info-name');
 let jobUser = document.querySelector('.profile__info-text');
 const formButton = popupElement.querySelector('.popup__form-btn');
-
-nameInput.value = nameUser.textContent;
-jobInput.value = jobUser.textContent;
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
@@ -45,4 +43,4 @@ function handleFormSubmit (evt) {
   closePopup();
 }
 
-formButton.addEventListener('click', handleFormSubmit);
+formElement.addEventListener('submit', handleFormSubmit);
